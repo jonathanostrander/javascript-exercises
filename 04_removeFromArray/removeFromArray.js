6c0a9084f0973e591e81a230b5387709067da9ff
jonathanostrander <57110848+jonathanostrander@users.noreply.github.com>
2022-12-08 10:27:03 -0600

    Did some exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (...args) {
+	let provideArray = args[0];
+	let newArray = [];
+	provideArray.forEach((item) => {
+		if (!args.includes(item)) {
+			newArray.push(item);
+		}
+	});
+	return newArray;
 };
 
 // Do not edit below this line
